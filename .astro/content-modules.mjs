33a8ee61707a645c5d83d60304916d253a6b6ec8
Miles Moore <79930428+milesalanmoore@users.noreply.github.com>
2026-06-25 08:06:08 -0600

    new deploy scripts

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,9 @@
-export default new Map();
+
+export default new Map([
+["src/content/news/2025-05-13-doe-csgf.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fnews%2F2025-05-13-doe-csgf.mdx&astroContentModuleFlag=true")],
+["src/content/news/2025-12-04-npr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fnews%2F2025-12-04-npr.mdx&astroContentModuleFlag=true")],
+["src/content/news/2025-09-05-sc25.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fnews%2F2025-09-05-sc25.mdx&astroContentModuleFlag=true")],
+["src/content/news/2026-01-13-sp26-classes.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fnews%2F2026-01-13-sp26-classes.mdx&astroContentModuleFlag=true")],
+["src/content/news/2025-12-15-fa25-classes.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fnews%2F2025-12-15-fa25-classes.mdx&astroContentModuleFlag=true")],
+["src/content/news/2025-12-09-clark-np.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fnews%2F2025-12-09-clark-np.mdx&astroContentModuleFlag=true")]]);
+		
